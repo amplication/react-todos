8229192221c3e0c5fa35d1725126b8ab369636b6
Michael Solati <mkslt04@gmail.com>
2022-03-15 20:46:40 -0700

    feat(step-001): add "Create the App"

---
+++ web/src/Task.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Task({ task, toggleCompleted }) {
+  const [completed, setCompleted] = useState(task.completed);
+
+  return (
+    <li className={completed ? "completed" : "incompleted"}>
+      <span>{task.text}</span>
+      <input
+        type="checkbox"
+        checked={completed}
+        onClick={() => toggleCompleted(task.id)}
+        onChange={() => setCompleted(task.completed)}
+        readOnly
+      />
+    </li>
+  );
+}
