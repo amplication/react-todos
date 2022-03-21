40fadee3c16942742c2cf50cfc21a0af5a719713
Michael Solati <mkslt04@gmail.com>
2022-03-20 21:02:19 -0700

    feat(step-005): add "Saving Tasks to the Backend"

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 import { me } from "./lib/auth";
+import * as tasksLib from "./lib/tasks";
 
 import Auth from "./Auth";
 import CreateTask from "./CreateTask";
@@ -11,30 +12,35 @@ function App() {
   const [user, setUser] = useState();
   const [tasks, setTasks] = useState([]);
 
+  const setUserFetchTasks = async (user) => {
+    setUser(user);
+    if (!user) return;
+    const result = await tasksLib.getAll(user.id);
+    setTasks(result);
+  };
+
   useEffect(() => {
     async function getUser() {
       const result = await me();
-      setUser(result);
+      setUserFetchTasks(result);
     }
     getUser();
-  }, [setUser]);
+  }, [setUser, setTasks]);
 
-  const createTask = (text, id) => ({
-    id,
-    text,
-    completed: false,
-  });
-
-  const addTask = (task) => {
+  const addTask = async (task) => {
+    const newTask = await tasksLib.create(task, user.id);
+    if (!newTask) return;
     const temp = [...tasks];
-    temp.push(createTask(task, tasks.length));
+    temp.push(newTask);
     setTasks(temp);
   };
 
-  const toggleCompleted = (id) => {
+  const toggleCompleted = async (task) => {
+    const updatedTask = await tasksLib.update(task);
+    if (!updatedTask) return;
     let temp = [...tasks];
-    const i = temp.findIndex((t) => t.id === id);
-    temp[i].completed = !temp[i].completed;
+    const i = temp.findIndex((t) => t.id === updatedTask.id);
+    temp[i] = updatedTask;
     setTasks(temp);
   };
 
@@ -46,7 +52,7 @@ function App() {
           <Tasks tasks={tasks} toggleCompleted={toggleCompleted} />
         </div>
       ) : (
-        <Auth setUser={setUser} />
+        <Auth setUser={setUserFetchTasks} />
       )}
     </div>
   );
