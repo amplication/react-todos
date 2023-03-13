75ab4df278cf73955c26e2041d4cd70bb9737717
Michael Solati <mkslt04@gmail.com>
2023-03-13 16:51:30 -0700

    feat(step-003): add "Adding Custom Endpoints"

--- server/src/auth/auth.resolver.ts
+++ server/src/auth/auth.resolver.ts
@@ -1,15 +1,16 @@
 import * as common from "@nestjs/common";
-import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver, Context } from "@nestjs/graphql";
+import { Request } from "express";
 import * as gqlACGuard from "../auth/gqlAC.guard";
 import { AuthService } from "./auth.service";
 import { GqlDefaultAuthGuard } from "./gqlDefaultAuth.guard";
 import { UserData } from "./userData.decorator";
 import { LoginArgs } from "./LoginArgs";
 import { UserInfo } from "./UserInfo";
-
+import { User } from "../user/base/User";
 @Resolver(UserInfo)
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
   @Mutation(() => UserInfo)
   async login(@Args() args: LoginArgs): Promise<UserInfo> {
     return this.authService.login(args.credentials);
@@ -19,5 +20,15 @@ export class AuthResolver {
   @common.UseGuards(GqlDefaultAuthGuard, gqlACGuard.GqlACGuard)
   async userInfo(@UserData() userInfo: UserInfo): Promise<UserInfo> {
     return userInfo;
+  }
+
+  @Mutation(() => UserInfo)
+  async signup(@Args() args: LoginArgs): Promise<UserInfo> {
+    return this.authService.signup(args.credentials);
+  }
+
+  @Query(() => User)
+  async me(@Context('req') request: Request): Promise<User> {
+    return this.authService.me(request.headers.authorization);
   }
 }
